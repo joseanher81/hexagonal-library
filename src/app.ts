7bfce2304633adbb84c4b03ef6871cfac340da54
José Ángel Hernández <joseangel@Mac-mini-de-Jose.local>
2025-05-23 20:27:23 +0200

    feat: added env variables

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import routes from "./adapters/api/routes";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.use("/api", routes);
 
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("Missing MONGO_URI in environment variables");
+}
+
 const start = async () => {
-  await mongoose.connect("mongodb://localhost:27017/library");
+  await mongoose.connect(MONGO_URI);
   app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
   });
